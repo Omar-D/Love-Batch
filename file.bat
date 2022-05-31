3169ac78b763055361c3337aa37b048d1e560f20
Omar-D <66242913+Omar-D@users.noreply.github.com>
2022-05-31 12:41:12 -0400

    Add files via upload

---
+++ file.bat
@@ -0,0 +1,10 @@
+@echo off
+color 04
+title WARNING FOR ONE
+rem echo I Love You Beyan Huri
+love2.jpg
+love3.jpg
+timeout /t 3 /nobreak
+start chrome https://www.youtube.com/watch?v=r5qUqyamVGM
+rem ./file.bat
+rem timeout /t 10 /nobreak
